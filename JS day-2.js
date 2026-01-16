4c4dcbb1a0e7c1a299adbed4d8f4dd0bf9520ac7
Aprajita <mishraaprajita43@gmail.com>
2026-01-16 20:44:49 +0530

    Initial commit

---
+++ JS day-2.js
@@ -0,0 +1,32 @@
+///ARRAY METHOD
+
+// 1.MAP METHOD
+let numbers=[1,2,3];
+let double=numbers.map((x)=>x*2); //We call it callback function.Map is a higher order method
+console.log(double);
+
+// 2.FILTER METHOD
+let marks=[7,23,34,35,26,37,4,0,45,33];
+let result=marks.filter((marks)=>marks>=16);
+console.log(result); 
+
+/* Q1 Take a array with 10 different marks and print the marks and the message of pass and fail */
+/*let marks=[33,22,44,11,23,34,45,20,39,47];
+let result=marks.filter((mark)=>{
+    if (mark>=16){
+        console.log(mark,"Pass")
+    }
+    else{
+        console.log(mark,"Fail")
+    }
+});*/
+
+// 3. REDUCE METHOD-Reduce an array to a single value.
+// Q2 Find out the sum of all the elements inside an array using for loop
+/*let arr=[1,2,3,4,5];
+let res=arr.reduce((acc,current)=>acc+current);
+console.log(res);*/
+// Q3 Difference between map,filter,reduce array methods.
+/* Map-same size of original array.
+Filter-subset of the original array.
+Reduce-only a asingle value.*/
---
+++ script.js
@@ -0,0 +1,48 @@
+//types of function
+//1. regular function
+
+function sample(){
+    console.log("sample")
+}
+sample()
+
+function sample(a){
+    console.log(a,"sample")
+}
+sample(5)
+ 
+function sample(a){
+    return "sample";
+}
+console.log(sample(5));
+
+//2. arrow function
+
+const sample=()=>console.log("sample")
+sample()
+
+const sample=()=>"sample"
+console.log(sample());
+
+const sample=(a,b)=>a+b;
+console.log(sample(5,10));
+
+//it doesn't need return statement and it follows asynchronous operation
+
+const sample=(a,b)=>{
+    return a+b;
+}
+console.log(sample(5,10))
+
+//3. callback function 
+//in callback fn if we pass a fn as an argument inside another fn
+//callback=demo()
+function sample(callback){
+    console.log("sample");
+    callback();
+}
+
+function demo(){
+    console.log("demo");
+}
+sample(demo);
