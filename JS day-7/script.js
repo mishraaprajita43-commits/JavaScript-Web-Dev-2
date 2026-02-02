0ba41d32188263f93082ae342c87f20db0d0f8df
Aprajita <mishraaprajita43@gmail.com>
2026-02-02 13:28:30 +0530

    js

--- JS day-7/script.js
+++ JS day-7/script.js
@@ -6,13 +6,17 @@ heading.style.color="tomato"
 heading.style.backgroundColor="yellow"
 heading.textContent="Hello Student!";
 
-//2. getElementByClassName
-const subHeading=document.getElementsByClassName("sub-heading")
-console.log(subHeading)
-for (let i=0;i<subHeading.length;i++){
-    subHeading[1].style.color="cyan"
-}
-//3.QUERY SELECTOR
-const content=document.querySelector("#content p")
-console.log(content);
-content.style.fontsize="40px"
+// //2. getElementByClassName
+// const subHeading=document.getElementsByClassName("sub-heading")
+// console.log(subHeading)
+// for (let i=0;i<subHeading.length;i++){
+//     subHeading[0].style.color="brown"
+//     subHeading[1].style.color="blue"
+//     subHeading[2].style.color="pink"
+//     subHeading[0].style.backgroundColorcolor="pink"
+
+// }
+// //3.QUERY SELECTOR
+// const content=document.querySelector("#content p")
+// console.log(content);
+// content.style.fontsize="40px"
---
+++ JS day-8/script.js
@@ -0,0 +1,14 @@
+//Task - 1
+const profile = document.getElementById("profile");
+profile.style.color="pink";
+profile.style.backgroundColor="black";
+profile.style.padding="20px";
+profile.style.textAlign="center"
+
+//Task-2
+const highlight = document.getElementsByClassName("important");
+console.log(highlight);
+for (let i=0; i<highlight.length; i++){
+    highlight[i].style.color="Blue";
+    highlight[i].style.fontSize="30px";
+}
